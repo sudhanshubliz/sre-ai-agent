6db2f3cc40790b17e6948174c6fbaa4ac2b428ba
Sudhanshu Ratna Thakur <sudhanshubliz@gmail.com>
2025-05-15 01:23:51 +0100

    openai

--- agent/agent_runner.py
+++ agent/agent_runner.py
@@ -1,17 +1,15 @@
 from langchain.chat_models import ChatOpenAI
 from dotenv import load_dotenv
-from groq import Groq
 import os
 
 def run_risk_agent(pr_summary):
     # Load environment variables from the .env file
+    load_dotenv()
     # Ensure the API key is correctly loaded
 
-    # Load API key securely
-    load_dotenv()
-    api_key = os.getenv("GROQ_API_KEY")
+    api_key = os.getenv("OPENAI_API_KEY")
 
-
+    
     if not api_key:
         raise RuntimeError("Environment variable OPENAI_API_KEY is not set.")
 
